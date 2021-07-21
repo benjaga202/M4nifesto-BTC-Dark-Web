5081801a4129790b1c9a2a4e172eb452a9b7d8ef
hack4lx <38162851+attakercyebr@users.noreply.github.com>
2021-07-21 12:25:24 +0000

    Add files via upload

---
+++ hack4lx.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ coding: utf-8 -*-
+📌️ M4nifest0 Black Hat Hacking Team™💪
+📌️ 𝕿𝖍𝖎𝖘 𝕴𝖘 𝕿𝖍𝖊 𝓜4𝓷𝓲𝓯𝓮𝓼𝓽0 𝕿𝖊𝖆𝖒™
+📌️ Author hack4lx 👊 
+📌 💪 Site : https://m4nifest0.com 👊
+📌 💪 Site : https://m4nifest0.group 👊
+📌 💪 Site : https://m4nifest0.shop 👊
+📌 💪 Channel Telegram : https://t.me/M4nifest0 👊
+📌 💪 Channel youtube : https://www.youtube.com/c/hack4lx 👊
+📌 2021 💪
+📌 Version 1.0.0 💪
+ __  __ _  _         _  __           _    ___  
+|  \/  | || |  _ __ (_)/ _| ___  ___| |_ / _ \ 
+| |\/| | || |_| '_ \| | |_ / _ \/ __| __| | | |
+| |  | |__   _| | | | |  _|  __/\__ \ |_| |_| |
+|_|  |_|  |_| |_| |_|_|_|  \___||___/\__|\___/ 
+    
+*/
+
+
+?>
+
+
+
